060706a804a2f1ae71eb2df4b6dd46cece903002
Palm <nipalm@f45c89acd433.ant.amazon.com>
2020-04-13 18:06:58 +0200

    Update manifest and add avg kill /  game

--- src/MainContent.js
+++ src/MainContent.js
@@ -16,13 +16,20 @@ const MainContent = () => {
     // Update the document title using the browser API
   }, []);
 
-  const RenderCard = ({ metric, transform }) => {
+  const RenderCard = ({ metric, transform, title }) => {
     let tempObj = {};
 
     // Fetch relevant stats from state
-    playerStats.forEach((player) => {
-      tempObj[player["gamer_tag"]] = player["stats"]["br"][metric];
-    });
+    if (metric === "avgKills") {
+      playerStats.forEach((player) => {
+        tempObj[player["gamer_tag"]] =
+          player["stats"]["br"]["kills"] / player["stats"]["br"]["gamesPlayed"];
+      });
+    } else {
+      playerStats.forEach((player) => {
+        tempObj[player["gamer_tag"]] = player["stats"]["br"][metric];
+      });
+    }
 
     // Sort
     let keysSorted = Object.keys(tempObj).sort(function (a, b) {
@@ -47,7 +54,7 @@ const MainContent = () => {
 
     return (
       <div className="Card">
-        <h3>{metric}</h3>
+        <h3>{title ? title : metric}</h3>
         <div className="Table">
           {keysSorted.map((player) => {
             return renderRow(player, tempObj[player]);
@@ -60,32 +67,44 @@ const MainContent = () => {
   const renderCards = () => {
     return (
       <div className="Wrapper">
-        <RenderCard metric="wins" />
-        <RenderCard metric="topFive" />
+        <RenderCard metric="wins" title="Wins" />
+        <RenderCard metric="topFive" title="# Top Five" />
+        <RenderCard
+          metric="avgKills"
+          title="Avg Kills/Game"
+          transform={(val) => {
+            if (val % 1 !== 0) {
+              return val.toFixed(2);
+            }
+          }}
+        />
         <RenderCard
           metric="timePlayed"
+          title="Time Played"
           transform={(val) => {
             return (val / 3600).toFixed(2) + "h";
           }}
         />
         <RenderCard
           metric="kdRatio"
+          title="K/D"
           transform={(val) => {
             if (val % 1 !== 0) {
               return val.toFixed(2);
             }
           }}
         />
-        <RenderCard metric="kills" />
+        <RenderCard metric="kills" title="Kills" />
         <RenderCard
           metric="scorePerMinute"
+          title="Score/min"
           transform={(val) => {
             if (val % 1 !== 0) {
               return val.toFixed(2);
             }
           }}
         />
-        <RenderCard metric="contracts" />
+        <RenderCard metric="contracts" title="Contracts" />
       </div>
     );
   };
